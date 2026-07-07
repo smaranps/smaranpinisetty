76b478ada93f32ca17b3726d0c8141a356d1cbb9
smaranps <smaranpinisetty@gmail.com>
2026-07-07 17:34:22 -0400

    Updated Text

--- app/page.tsx
+++ app/page.tsx
@@ -157,19 +157,9 @@ export default function Home() {
           }}
           className={googleSans.className}
         >
-          I live in Canada and I am currently in grade 10. I started coding when
-          I was in grade 3. I was mainly into block coding in Scratch. Soon, I
-          was introduced to web development by my mom and created my first
-          website when I was 10. I got my first computer, a MacBook Pro at the
-          age of 13. This led me to discovering React. From there, I learned how
-          to create multiple full stack applications which can benefit my
-          community. When I became 14, I discovered iOS development and SwiftUI.
-          Although it was challenging at first, I kept learning until I became
-          good at it. As of right now, I am developing my app and continuing to
-          attend hackathons to gain experience. Not only that, I am very
-          consistent academically by maintaining a 95 average throughout high
-          school and a part of multiple clubs such as DECA, ModelUN, Band and
-          more.
+         I live in Canada and am currently entering Grade 11. I started coding when I was in Grade 3, mainly using Scratch for block-based programming. Soon after, my mom introduced me to web development, and I created my first website when I was 10 years old. I got my first computer, a MacBook Pro, at the age of 13, which led me to discover React. From there, I learned how to build multiple full-stack applications that could benefit my community.
+When I turned 14, I discovered iOS development and SwiftUI. Although it was challenging at first, I kept learning until I became proficient. As of now, I am continuing to develop my app and attend hackathons to gain more experience. In addition, I have maintained a 95% average throughout high school while actively participating in several clubs, including DECA, Model UN, Band, and more.
+
         </motion.p>
 
         <motion.h2
